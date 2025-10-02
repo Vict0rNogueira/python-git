401d7357189d2ccdd495dbfb11ec4f748a1033e4
VictorNogueira <Victorndecastro@outlook.com>
2025-10-01 22:07:18 -0300

    FastAPI

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-from fastapi import FastAPI
+from fastapi import FastAPI, HTTPException
+from pydantic import BaseModel
 
 app = FastAPI()
 
@@ -13,3 +14,42 @@ def rota_teste():
     return {
         "message": "Ta funcionando"
     }
+
+class Produto(BaseModel):
+    id: int
+    nome: str
+
+produtos = []
+
+@app.get ("/produtos")
+def get_produtos():
+    return produtos
+
+@app.post ("/produtos")
+def post_produtos(item: Produto):
+
+    for produto in produtos:
+        if produto.id == item.id:
+            raise HTTPException(status_code=400, detail="Item já cadastrado")
+
+    produtos.append(item)
+    return item
+
+@app.patch ("/produtos/{id}")
+def patch_produtos(item: Produto, id:int):
+    indice_produto = None
+    for i in range(len(produtos)):
+        produto = produtos[i]
+        if produto.id == id:
+            indice_produto = i
+            break
+    if indice_produto == None:
+        raise HTTPException(status_code=404, detail="Produto não encontrado")
+    
+    produtos[i] = item
+    return item
+
+@app.delete ("/produtos/{id}")
+def delete_produtos():
+    return{}
+
